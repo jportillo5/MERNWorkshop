7b562774f3568cabebc81fbd94e31414c3e4a899
jportillo5 <jtsportillo@gmail.com>
2024-04-07 17:22:03 -0400

    Express app fully setup

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,17 @@ const express = require('express');
 //Register our express app
 const app = express();
 
+//register some middleware - any code executed between getting a request for the server, and us sending a response
+app.use((req, res, next) => { //request coming in, response going out, what gets ran at the end of this method
+    console.log(req.path, req.method);
+    next(); //the next piece of middleware (our routes)
+});
+
+//React to requests by establishing routes; this will all be replaced later
+app.get('/', (req, res) => { //request coming in, response going out
+    res.json({msg: 'Welcome to the app'}); //use the response object to send back a json string
+});
+
 //Listen for requests at a certain port number
 app.listen(4000, () => {
     console.log('listening on port 4000');
